b833e6e84a9a8a53f88ffefebbc03f6841eb2bee
Nguyen Khac Nhat <nhat@agilead.vn>
2018-02-21 16:36:04 +0700

    Fix naming

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,8 +1,8 @@
 public class Cylinder {
-    public static double getArea(int radius, int height){
+    public static double getVolume(int radius, int height){
         double baseArea = Math.PI * radius * radius;
         double perimeter = 2 * Math.PI  * radius;
-        double area = perimeter * height + 2 * baseArea;
-        return area;
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
     }
 }
--- test/CylinderTest.java
+++ test/CylinderTest.java
@@ -6,24 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class CylinderTest {
 
     @Test
-    @DisplayName("Testing getArea(0, 0)")
-    void testGetArea0And0() {
+    @DisplayName("Testing getVolume(0, 0)")
+    void testGetVolume0And0() {
         int radius = 0;
         int height = 0;
         double expected = 0;
 
-        double result = Cylinder.getArea(radius, height);
+        double result = Cylinder.getVolume(radius, height);
         assertEquals(expected, result);
     }
 
     @Test
-    @DisplayName("Testing getArea(1, 2)")
-    void testGetArea1And2() {
+    @DisplayName("Testing getVolume(1, 2)")
+    void testGetVolume1And2() {
         int radius = 1;
         int height = 2;
         double expected = 18.84955592153876;
 
-        double result = Cylinder.getArea(radius, height);
+        double result = Cylinder.getVolume(radius, height);
         assertEquals(expected, result);
     }
 }
